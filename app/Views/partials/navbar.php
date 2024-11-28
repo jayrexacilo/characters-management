8f5646e5f282f7c1585c2fea985b081c32c6cc5a
jayrex <acilojayrex@gmail.com>
2024-11-28 15:08:49 +0800

    refactor nav menu

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -6,29 +6,23 @@
         <img src="https://placehold.co/120x40?text=Hello+World" alt="Logo" width="120" height="40" class="d-inline-block align-text-top">
       </a>
       <ul class="navbar-nav ms-auto">
-        <?php if(uri_string() == "login" || !session()->get('is_logged_in')) { ?>
+        <?php if(!session()->get('is_logged_in')): ?>
             <li class="nav-item me-4">
                 <a class="nav-link" href="<?= route_to('AuthController::signup') ?>">Sign Up</a>
             </li>
-        <?php } ?>
-        <?php if(uri_string() == "sign-up" || !session()->get('is_logged_in')) { ?>
             <li class="nav-item me-4">
                 <a class="nav-link" href="<?= route_to('AuthController::login') ?>">Log In</a>
             </li>
-        <?php } ?>
-        <?php if (session()->get('is_logged_in')): ?>
+        <?php else: ?>
             <li class="nav-item me-4">
               <a class="nav-link" href="<?= route_to('Character::index') ?>">List Characters</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="<?= route_to('Character::savedCharacters') ?>">Saved Characters</a>
             </li>
-
-            <?php if (session()->get('is_logged_in')): ?>
-                <li class="nav-item">
-                  <a class="nav-link" href="<?= route_to('AuthController::logout') ?>">Log Out</a>
-                </li>
-            <?php endif; ?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= route_to('AuthController::logout') ?>">Log Out</a>
+            </li>
         <?php endif; ?>
 
       </ul>
